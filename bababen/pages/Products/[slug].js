b8c3bf42fec6ed0f8731905d78060ce53988ece9
Abdul Sami <abdulsamisiddiq2001@gmail.com>
2022-08-31 23:26:34 +0500

    .

--- bababen/pages/Products/[slug].js
+++ bababen/pages/Products/[slug].js
@@ -100,7 +100,7 @@ const Slug = () => {
                 <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
                 <button onClick={() => addItemToCart({
                   Key: 1,
-                  Name: "Add to Cart",
+                  Name: "Add to Cart.",
                   Size: 'M, S',
                   Variant: 'Green, Blue',
                 },1)} className="flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Add to Cart</button>
